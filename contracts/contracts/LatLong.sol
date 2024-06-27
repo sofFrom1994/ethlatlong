928655f6df761076832bb21f7250e9579e652107
SofFrom1994 <sofFrom1994@protonmail.com>
2024-06-27 17:53:14 -0500

    addLayer assert, getLayer, getEmbeds

--- contracts/contracts/LatLong.sol
+++ contracts/contracts/LatLong.sol
@@ -22,8 +22,10 @@ contract LatLong {
   struct Layer {
     string name;
     string description;
+    // maybe owner / have a permissioned layer
     uint embedN;
     Embed[] embeds;
+    Location startingLoc;
   }
 
   //constants
@@ -31,46 +33,57 @@ contract LatLong {
 
   mapping (string => Layer) layers;
 
-// Define a new event to notify when a layer is added
-event LayerAdded(string name, string description);
+  // Define a new event to notify when a layer is added
+  event LayerAdded(string name, string description);
+  
+  // Define a new event to notify when a message is added
+  event EmbedAdded(string layer, string content, SD59x18 lat, SD59x18 long);
 
-// Define a new event to notify when a message is added
-event EmbedAdded(string layer, string content, SD59x18 lat, SD59x18 long);
+  function getLayer( string calldata name) public view returns (Layer memory) {
+    Layer memory layer = layers[name];
+    // check if layer exists
+    require(bytes(layer.name).length != 0);
+    return layer;
+  }
 
-function addLayer(string calldata name, string calldata description) public {
-    // Check if the layer already exists
-    require(bytes(layers[name].name).length == 0, "Layer already exists");
-
-    // Initialize the new layer
-    Layer storage newLayer = layers[name];
-    newLayer.name = name;
-    newLayer.description = description;
-
-    // Emit event for adding a layer
-    emit LayerAdded(name, description);
-}
-
-function addMessage(
-    string calldata layerName,
-    SD59x18 lat,
-    SD59x18 long,
-    string calldata message
-) public {
-
-    // Check if the layer exists
-    require(bytes(layers[layerName].name).length != 0, "Layer does not exist");
-    // Check if lat and long are valid
-    require(lat.gt(sd(-90e18)) && lat.lt(sd(90e18)) && long.gt(sd(-180)) && long.lt(sd(180)), "Invalid location");
-
-    Location memory location = Location(lat, long);
+  function getEmbeds(string calldata layerName) public view returns (Embed[] memory embeds) {
     Layer memory layer = layers[layerName];
-    Embed memory embed = Embed(layer.embedN++, Kinds.Message, message, location);
+      // check if layer exists
+    require(bytes(layer.name).length != 0);
 
-    layers[layerName].embeds.push(embed);
-}
+    return layer.embeds;
+  }
+  
+  function addLayer(string calldata name, string calldata description, Location calldata startingLocation) public {
+      require(bytes(layers[name].name).length == 0, "Layer already exists");
+      require(startingLocation.lat.gt(sd(-90e18)) && startingLocation.lat.lt(sd(90e18)) && startingLocation.long.gt(sd(-180)) && startingLocation.long.lt(sd(180)), "Invalid location");
+  
+      // Initialize the new layer
+      Layer storage newLayer = layers[name];
+      newLayer.name = name;
+      newLayer.description = description;
+      newLayer.startingLoc = startingLocation;
+  
+      // Emit event for adding a layer
+      emit LayerAdded(name, description);
+  }
+  
+  function addMessage(
+      string calldata layerName,
+      SD59x18 lat,
+      SD59x18 long,
+      string calldata message
+  ) public {
+      // Check if lat and long are valid
+      require(lat.gt(sd(-90e18)) && lat.lt(sd(90e18)) && long.gt(sd(-180)) && long.lt(sd(180)), "Invalid location");
+      Layer memory layer = layers[layerName];
 
-function getEmbeds(string calldata layerName) public {
-  //
-}
-
+      // check if layer exists
+      require(bytes(layer.name).length != 0);
+  
+      Location memory location = Location(lat, long);
+      Embed memory embed = Embed(layer.embedN++, Kinds.Message, message, location);
+  
+      layers[layerName].embeds.push(embed);
+  }
 }
