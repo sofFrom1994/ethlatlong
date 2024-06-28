a786d2d086451bc42e62f7da7bb41f229612c9c9
SofFrom1994 <sofFrom1994@protonmail.com>
2024-06-28 02:59:09 -0500

    simple module skeleton

--- contracts/ignition/modules/EthLatLong.ts
+++ contracts/ignition/modules/EthLatLong.ts
@@ -1,8 +1,8 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const LockModule = buildModule("EthLatLongModule", (m) => {
+const EthLatLongModule = buildModule("EthLatLongModule", (m) => {
   const ell = m.contract("EthLatLong");
   return { ell };
 });
 
-export default LockModule;
+export default EthLatLongModule;
