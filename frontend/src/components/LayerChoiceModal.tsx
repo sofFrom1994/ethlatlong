c7b589ead8118daad88f25a64f5d252deba926ce
SofFrom1994 <sofFrom1994@protonmail.com>
2024-06-28 18:03:15 -0500

    get a particular layer

---
+++ frontend/src/components/LayerChoiceModal.tsx
@@ -0,0 +1,29 @@
+import { abi } from "./ethLatLongABI";
+
+import { useReadContract } from 'wagmi'
+
+export const getLayer = () => {
+  const { 
+    data,
+    error,
+    isPending
+  } = useReadContract({
+    abi,
+    address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    functionName: 'getLayer',
+    args: ["layer 1"],
+  })
+
+  if (isPending) return <div>Loading...</div>
+
+  if (error)
+    return (
+      <div>
+        Error: {(error).shortMessage || error.message}
+      </div>
+    )
+
+  return (
+    <div>Layer: {data?.toString()}</div>
+  )
+}
