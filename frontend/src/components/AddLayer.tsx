830572366e46e83bbccf681c61e8b6ce209ad183
SofFrom1994 <sofFrom1994@protonmail.com>
2024-06-28 17:51:17 -0500

    add layer and message skeletons

--- frontend/src/components/AddLayer.tsx
+++ frontend/src/components/AddLayer.tsx
@@ -11,19 +11,19 @@ export function AddMessage() {
     const lat = formData.get("lat") as string;
     const long = formData.get("long") as string;
     const layerName = formData.get("layerName") as string;
-    const message = formData.get("message") as string;
+    const description = formData.get("description") as string;
 
     writeContract({
       address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
       abi,
-      functionName: "addMessage",
-      args: [layerName, BigInt(lat), BigInt(long), message],
+      functionName: "addLayer",
+      args: [layerName, description, BigInt(lat), BigInt(long)],
     });
   }
   return (
     <form onSubmit={submit}>
-      <input name="message" placeholder="hello world" required />
       <input name="layerName" placeholder="layer 1" required />
+      <input name="description" placeholder="hello world" required />
       <input name="lat" placeholder="54" required />
       <input name="long" placeholder="-89" required />
       <button type="submit">Post</button>
