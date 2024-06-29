60c035f0d751a969f1fda320bd9f987fa3969099
SofFrom1994 <sofFrom1994@protonmail.com>
2024-06-29 03:17:33 -0500

    initialize one layer and one message

--- contracts/ignition/modules/EthLatLong.ts
+++ contracts/ignition/modules/EthLatLong.ts
@@ -2,6 +2,13 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const EthLatLongModule = buildModule("EthLatLongModule", (m) => {
   const ell = m.contract("EthLatLong");
+  m.call(ell, "addLayer", [
+    "layer 1", " just a layer", 83, 83
+  ]);
+  m.call(ell, "addMessage", [
+    "layer 1", 85, 83, "this is a message"
+  ]);
+
   return { ell };
 });
 
