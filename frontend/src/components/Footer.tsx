93df5ff574905d759bad5469b6ee8de3df308e5f
SofFrom1994 <sofFrom1994@protonmail.com>
2024-07-10 12:38:10 -0500

    using simple popover for footer info.

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,11 +1,32 @@
+import { DialogTrigger, Button, Popover, OverlayArrow, Dialog, Heading } from "react-aria-components"
 import "../styles/Footer.css"
+
+const infoPopover = (buttonLabel : string, title : string, descrition : string) => {
+  return (
+
+<DialogTrigger>
+  <Button aria-label="">{buttonLabel}</Button>
+  <Popover>
+    <OverlayArrow>
+      <svg width={12} height={12} viewBox="0 0 12 12">
+        <path d="M0 0 L6 6 L12 0" />
+      </svg>
+    </OverlayArrow>
+    <Dialog>
+      <Heading slot="title">{title}</Heading>
+      <p>{descrition}</p>
+    </Dialog>
+  </Popover>
+</DialogTrigger>
+  )
+}
 
 export const Footer = () => {
   return (
         <footer>
-          <p> About </p>
-          <p> Legend </p>
-          <p> Roadmap </p>
+          {infoPopover ("About", "About", "about")}
+          {infoPopover ("Legend", "Legend", "about")}
+          {infoPopover ("Roadmap", "Roadmap", "roadmap")}
         </footer>
 )
 }
