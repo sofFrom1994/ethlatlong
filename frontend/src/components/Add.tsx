1405a6a7f031b1d2aca2816831490bf3f8411d0a
SofFrom1994 <sofFrom1994@protonmail.com>
2024-07-10 00:35:56 -0500

    addmenu bugfix : modal open state wasn't being reset

--- frontend/src/components/Add.tsx
+++ frontend/src/components/Add.tsx
@@ -4,6 +4,7 @@ import { AddMessageRA } from './AddMessageRA';
 import { AddLayerRA } from './AddLayerRA';
 import "../styles/Add.css";
 
+/*
 const AddModal = ({ children }) => {
   const [isOpen, setOpen] = useState(true);
 
@@ -49,6 +50,61 @@ export const AddMenu = () => {
         </Popover>
       </MenuTrigger>
       {modalContent && <AddModal>{modalContent}</AddModal>}
+    </>
+  );
+}
+
+*/
+
+const AddModal = ({ children, isOpen, onOpenChange }) => {
+  return (
+    <DialogTrigger isOpen={isOpen} onOpenChange={onOpenChange}>
+      <Modal isDismissable>
+        <Dialog>
+          <Heading>Add</Heading>
+          {children}
+        </Dialog>
+      </Modal>
+    </DialogTrigger>
+  );
+}
+
+export const AddMenu = () => {
+  const [modalContent, setModalContent] = useState<React.ReactNode>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleAction = (key: React.Key) => {
+    if (key === "layer") {
+      setModalContent(<AddLayerRA />);
+    } else if (key === "message") {
+      setModalContent(<AddMessageRA />);
+    } else if (key === "media") {
+      alert("Can't add media via UI yet");
+    } else {
+      console.log("Invalid choice: ", key);
+    }
+    setIsModalOpen(true);
+  };
+
+  return (
+    <>
+      <MenuTrigger>
+        <Button aria-label="Menu">
+          <div className="plus"> + </div>
+        </Button>
+        <Popover>
+          <Menu onAction={handleAction}>
+            <MenuItem id="layer">Layer</MenuItem>
+            <MenuItem id="message">Message</MenuItem>
+            <MenuItem id="media">Media</MenuItem>
+          </Menu>
+        </Popover>
+      </MenuTrigger>
+      {modalContent && (
+        <AddModal isOpen={isModalOpen} onOpenChange={setIsModalOpen}>
+          {modalContent}
+        </AddModal>
+      )}
     </>
   );
 }
