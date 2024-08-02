d76b15e7cc8437d36a8c1097a11d7012b1e8f849
SofFrom1994 <sofFrom1994@protonmail.com>
2024-08-02 18:50:53 -0500

    optionally set icon size for leaflet colored icons

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -6,12 +6,12 @@ export const parseLatLong = (latlong : string) => {
   return parseUnits(latlong, 18)
 }
 
-export const coloredIcon = (color: Color, svg: string) => {
+export const coloredIcon = (color: Color, svg: string, iconSize : L.PointExpression = [25, 41]) => {
   const htmlS = `<span style="color: #${color};">${svg}</span>`;
   return L.divIcon({
     iconUrl: svg,
     className: "emptyDummy",
-    iconSize: [25, 41],
+    iconSize: iconSize,
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
     html: htmlS
