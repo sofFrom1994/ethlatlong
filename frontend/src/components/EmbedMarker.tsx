951040b5597975f80c09b77279ae566215530f0a
SofFrom1994 <sofFrom1994@protonmail.com>
2024-08-02 18:09:34 -0500

    move coloredIcon function to utils.ts

--- frontend/src/components/EmbedMarker.tsx
+++ frontend/src/components/EmbedMarker.tsx
@@ -1,38 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { useReadContract } from "wagmi";
-import { ethLatLongAbi } from "../generated";
-import { LayerGroup, LayersControl, Marker, Popup } from "react-leaflet";
+import { Marker, Popup } from "react-leaflet";
 import { LatLngExpression } from "leaflet";
 import L from "leaflet";
 
 import messageSVG from "../assets/message-outline.svg?raw";
 import mediaSVG from "../assets/photo.svg?raw";
-import farCastIcon from "../assets/purple-white.svg";
 import { Color, embedType, layerType } from './types';
-
-const coloredIcon = (color: Color, svg: string) => {
-  const htmlS = `<span style="color: #${color};">${svg}</span>`;
-  return L.divIcon({
-    iconUrl: svg,
-    className: "emptyDummy",
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    html: htmlS
-  });
-}
-
-const messageDiv = coloredIcon("#12F", messageSVG); 
-const mediaIcon = coloredIcon("#F2F", mediaSVG); 
-
-const farCast = L.icon({
-  iconUrl: farCastIcon,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34]
-  //className: "markerColor"
-})
-
+import { coloredIcon } from '../utils';
 
 const nToColor = (nColor : number) => {
   return nColor.toString(16) as Color;
@@ -48,7 +21,6 @@ export const embedToMarker = (layer: layerType, embed : embedType) => {
     // todo: path icon
     embedIcon = coloredIcon(markerColor, messageSVG);
   } else if (embed.kind === 2) {
-    embedIcon = mediaIcon;
     embedIcon = coloredIcon(markerColor, mediaSVG);
   } else {
     // unknown marker
--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,5 +1,19 @@
 import { parseUnits } from 'viem'
+import { Color } from './components/types';
+import L from 'leaflet';
 
 export const parseLatLong = (latlong : string) => {
   return parseUnits(latlong, 18)
+}
+
+export const coloredIcon = (color: Color, svg: string) => {
+  const htmlS = `<span style="color: #${color};">${svg}</span>`;
+  return L.divIcon({
+    iconUrl: svg,
+    className: "emptyDummy",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    html: htmlS
+  });
 }
