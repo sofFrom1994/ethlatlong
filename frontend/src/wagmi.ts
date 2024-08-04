9995eae692ab7cfcb322029b2690ce5906aa6890
SofFrom1994 <sofFrom1994@protonmail.com>
2024-08-04 17:13:04 -0500

    not logging project id anymore

--- frontend/src/wagmi.ts
+++ frontend/src/wagmi.ts
@@ -3,7 +3,6 @@ import { http } from 'viem'
 import { baseSepolia, hardhat } from 'wagmi/chains';
 
 const projectId = import.meta.env.VITE_WALLETCONNECT_PROJECT_ID;
-console.log(projectId);
 const appURL = import.meta.env.VITE_APP_URL;
 
 export const config = getDefaultConfig({
