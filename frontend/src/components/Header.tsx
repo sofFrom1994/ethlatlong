ee3005d42aa522ee7a8492faebc75cabdb3f79a5
SofFrom1994 <sofFrom1994@protonmail.com>
2024-08-06 18:26:55 -0500

    separator only conditionally rendered

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,17 @@
-import "../styles/Header.css"
-import { ConnectButton } from '@rainbow-me/rainbowkit';
+import "../styles/Header.css";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { UserTimeline } from "./UserTimeline";
 import { Config, UseAccountReturnType } from "wagmi";
 
-export const Header = (props: {account : UseAccountReturnType<Config> }) => {
+export const Header = (props: { account: UseAccountReturnType<Config> }) => {
+  let separator = null;
+
+  if (props.account.isConnected) {
+    separator = () => {
+      return <div className="separator" >&nbsp;</div>;
+    };
+  }
+
   return (
     <header>
       <div className="header-content">
@@ -12,8 +20,8 @@ export const Header = (props: {account : UseAccountReturnType<Config> }) => {
           <ConnectButton
             label="Sign in"
             accountStatus={{
-              smallScreen: 'avatar',
-              largeScreen: 'full'
+              smallScreen: "avatar",
+              largeScreen: "full",
             }}
             showBalance={{
               smallScreen: false,
@@ -21,7 +29,8 @@ export const Header = (props: {account : UseAccountReturnType<Config> }) => {
             }}
           />
         </div>
-          <UserTimeline account={props.account}/>
+          {separator && separator()}
+        <UserTimeline account={props.account} />
       </div>
     </header>
   );
