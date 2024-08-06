db3d7089ce5ab00d161531ebaf8fc5cd7d513b1c
SofFrom1994 <sofFrom1994@protonmail.com>
2024-08-06 13:41:42 -0500

    delete message/embed if author UI

--- frontend/src/components/EmbedMarker.tsx
+++ frontend/src/components/EmbedMarker.tsx
@@ -6,13 +6,30 @@ import messageSVG from "../assets/message-outline.svg?raw";
 import mediaSVG from "../assets/photo.svg?raw";
 import { Color, embedType, layerType } from './types';
 import { coloredIcon } from '../utils';
+import { Config, UseAccountReturnType } from "wagmi";
+import { ethLatLongAbi } from "../generated";
+
+const abi = ethLatLongAbi;
+const contract_address = import.meta.env.VITE_CONTRACT_ADDRESS;
 
 const nToColor = (nColor : number) => {
   return nColor.toString(16) as Color;
 }
 
-export const embedToMarker = (layer: layerType, embed : embedType) => {
+export const embedToMarker = (layer: layerType, embed : embedType,  account: UseAccountReturnType<Config> ) => {
   const markerColor = nToColor(layer.color);
+  const deleteMarker = () => {
+    console.log("removing message...");
+    /*
+    writeContract({
+      address: contract_address,
+      abi,
+      functionName: "removeMessage",
+      args: [layer.name, embed.id],
+    });
+    */
+  }
+
   let embedIcon: L.Icon<L.IconOptions> | L.DivIcon ;
   //
   if (embed.kind === 0) {
@@ -27,6 +44,23 @@ export const embedToMarker = (layer: layerType, embed : embedType) => {
     embedIcon = coloredIcon(markerColor, messageSVG);
   }
 
+  let deleteButton = null;
+  if (account.isConnected) {
+    console.log("account connected");
+    if (String(account.address) === embed.author) {
+      console.log("same author");
+      deleteButton = () => {
+        return (
+          <button
+            style={{ backgroundColor: "red" }}
+            onClick={() => deleteMarker()}
+          >
+            Delete
+          </button>
+        );
+      };
+    }
+  }
   return (
     <div>
       <Marker
@@ -43,6 +77,7 @@ export const embedToMarker = (layer: layerType, embed : embedType) => {
           {embed.message}
           <br />
           by {embed.author}
+        {deleteButton && deleteButton()}
         </Popup>
       </Marker>
     </div>
