cfe6e58a62e22150920d15da8a070a4f0c409972
SofFrom1994 <sofFrom1994@protonmail.com>
2024-08-07 23:59:22 -0500

    explicit icon anchors

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -6,12 +6,13 @@ export const parseLatLong = (latlong : string) => {
   return parseUnits(latlong, 18)
 }
 
-export const coloredIcon = (color: Color, svg: string, iconSize : L.PointExpression = [15, 15]) => {
+export const coloredIcon = (color: Color, svg: string, iconSize : L.PointExpression = [15, 15], iconAnchor : L.PointExpression = [9, 15]) => {
   const htmlS = `<span style="color: #${color};">${svg}</span>`;
   return L.divIcon({
     iconUrl: svg,
     className: "colored-icon",
     iconSize: iconSize,
+    iconAnchor: iconAnchor,
     html: htmlS
   });
 }
