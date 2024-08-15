fd838b558f919dafdbd2bf293b4053fb5e0b7e9a
SofFrom1994 <sofFrom1994@protonmail.com>
2024-08-15 00:57:05 -0500

    Footer refactor, more legend styling.

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,65 +1,100 @@
-import { DialogTrigger, Button, Popover, OverlayArrow, Dialog, Heading } from "react-aria-components"
-import "../styles/Footer.css"
-import { ReactNode } from "react"
+import {
+  DialogTrigger,
+  Button,
+  Popover,
+  OverlayArrow,
+  Dialog,
+  Heading,
+  Modal,
+} from "react-aria-components";
+import "../styles/Footer.css";
 
-import messageIcon from "../assets/message-outline.svg"
-import mediaIcon from "../assets/photo-outline.svg"
-import castIcon from "../assets/purple-white.svg"
-
-const infoPopover = (buttonLabel : string, title : string, content : ReactNode) => {
-  return (
-
-<DialogTrigger>
-  <Button className="footer-button" aria-label="">{buttonLabel}</Button>
-  <Popover style={{zIndex: 240000001}}>
-    <OverlayArrow>
-      <svg width={12} height={12} viewBox="0 0 12 12">
-        <path d="M0 0 L6 6 L12 0" />
-      </svg>
-    </OverlayArrow>
-    <Dialog>
-      <Heading slot="title">{title}</Heading>
-      {content}
-    </Dialog>
-  </Popover>
-</DialogTrigger>
-  )
-}
-
-const roadmap = () => {
-  const roadmap = () => {
-    return (
-      <ul className="roadmap">
-        <li> some element</li>
-        <li> some element</li>
-        <li> some element</li>
-        <li> some element</li>
-      </ul>
-    )
-  }
-
-  return infoPopover("Roadmap", "Roadmap", roadmap())
-}
+import messageIcon from "../assets/message.svg?raw";
+import mediaIcon from "../assets/photo-outline.svg?raw";
+import castIcon from "../assets/purple-white.svg?raw";
+import layerIcon from "../assets/map.svg?raw"
+import { coloredSVG } from "../utils";
 
 const legend = () => {
-  const legend = () => {
-    return (
-      <div className="legend">
-        <div className="legend-symbol"><img src={messageIcon} alt="" />Messages</div>
-        <div className="legend-symbol"><img src={mediaIcon} alt="" />Media</div>
-        <div className="legend-symbol"><img src={castIcon} alt="" />Casts</div>
-      </div>
-    );
-  };
-  return infoPopover("Legend", "Legend", legend());
+  return (
+    <DialogTrigger>
+      <Button className="footer-button" aria-label="">Legend</Button>
+      <Popover style={{ paddingBottom: "2.5rem", width: "10rem", height: "13rem", zIndex: 240000001 }}>
+        <OverlayArrow>
+          <svg width={12} height={12} viewBox="0 0 12 12">
+            <path d="M0 0 L6 6 L12 0" />
+          </svg>
+        </OverlayArrow>
+        <Dialog>
+          <h2> Legend </h2>
+          <div className="legend">
+            <div className="legend-symbol">
+              {coloredSVG("#FFFFFF", messageIcon)}
+              <h3> Messages  </h3>
+            </div>
+            <div className="legend-symbol">
+              {coloredSVG("#FFFFFF", mediaIcon)}
+              <h3> Media </h3>
+            </div>
+            <div className="legend-symbol">
+              {coloredSVG("none", castIcon)}
+              <h3> Casts </h3>
+            </div>
+            <div className="legend-symbol">
+              {coloredSVG("#FFFFFF", layerIcon)}
+              <h3> Layers </h3>
+            </div>
+          </div>
+        </Dialog>
+      </Popover>
+    </DialogTrigger>
+  );
+};
+
+ 
+const roadmap = () => {
+  return (
+    <DialogTrigger>
+      <Button className="footer-button" aria-label="">
+        Roadmap{" "}
+      </Button>
+      <Modal isDismissable>
+        <Dialog>
+          <Heading slot="title">roadmap</Heading>
+          <ul className="roadmap">
+            <li> some element</li>
+            <li> some element</li>
+            <li> some element</li>
+            <li> some element</li>
+          </ul>
+        </Dialog>
+      </Modal>
+    </DialogTrigger>
+  );
+};
+
+const about = () => {
+  return (
+    <DialogTrigger>
+      <Button className="footer-button" aria-label="">
+        About
+      </Button>
+      <Modal isDismissable>
+        <Dialog>
+          <Heading slot="title">about</Heading>
+          about
+        </Dialog>
+      </Modal>
+    </DialogTrigger>
+  );
 };
 
 export const Footer = () => {
   return (
-        <footer>
-          {infoPopover ("About", "About", "lengthy description about the app")}
-          {legend()}
-          {roadmap()}
-        </footer>
-)
-}
+    <footer>
+      {legend()}
+      {about()}
+      {roadmap()}
+    </footer>
+  );
+};
